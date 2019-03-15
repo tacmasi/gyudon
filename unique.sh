73dd872ab1f8788ee0ecb725a19da0a8a58c33d6
tacmasi <taka.massy@gmail.com>
2019-03-16 07:39:27 +0900

    20190316

--- unique.sh
+++ unique.sh
@@ -1,3 +1,6 @@
 head -1 all_gyudon_colnameon.csv|awk -F, '{print $NF}' > unique.csv
 echo Quantity Gyudon_Wage>> unique.csv
 awk -F, '$NF != "NA"{print $NF}' all_gyudon.csv |sort -n|uniq -c >> unique.csv
+head -1 all_gyudon_colnameon.csv|awk -F, '{print $NF}' > except_tokyo.csv
+echo Quantity Gyudon_Wage_except_Tokyo_Kanagawa_Chiba_Saitama>> except_tokyo.csv
+cat all_gyudon.csv|grep -v 東京都|grep -v 神奈川|grep -v 埼玉県 |grep -v 千葉県|awk -F, '$NF != "NA"{print $NF}' |sort -n|uniq -c >> except_tokyo.csv
